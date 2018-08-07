8d1f47b45a61f5558f839eeb26feac766c0a2cbe
Mark Pena <markdrew53@gmail.com>
2018-08-07 00:32:21 -0500

    Movements
    the logic of the player's movement was added

---
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private float horizontalInput;
+    private float verticalInput;
+    [SerializeField]
+    private float speed;
+
+    private void Movements()
+    {
+        speed = 5.0f;
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+
+        // Player Bounds
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x > 8)
+        {
+            transform.position = new Vector3(8, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8)
+        {
+            transform.position = new Vector3(-8, transform.position.y, 0);
+        }
+    }
+    
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movements();
+	}
+
+}
+    
