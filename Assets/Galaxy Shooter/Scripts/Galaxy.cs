64148736541acae345526a3962474dd931955c05
Mark Pena <markdrew53@gmail.com>
2018-08-20 01:24:02 -0500

    the galaxy script was added :)
    Is responsible for playing the song of the fund, once the player decides to start the game.

---
+++ Assets/Galaxy Shooter/Scripts/Galaxy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Galaxy : MonoBehaviour {
+
+    // Audio Source
+    private AudioSource _musicBackground;
+
+    private void BackgroundSong() {
+        _musicBackground = GetComponent<AudioSource>();
+        _musicBackground.Play();
+    }
+
+	// Use this for initialization
+	void Start () {
+        BackgroundSong();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
